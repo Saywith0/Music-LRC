a7f0e222729f344f60f70f0a8cd214467bdaadea
Saywith0 <121112951@qq.com>
2023-12-13 15:43:04 +0800

    Signed-off-by: Saywith0 <121112951@qq.com>

---
+++ js/data.js
@@ -0,0 +1,135 @@
+var lrc = `[00:00]ALL MY PEOPLE - Alexandra Stan
+[00:03]Sometimes I feel you on my body
+[00:05]我总能感觉的到你的存在
+[00:05]You make my head spin yeah oh oh oh
+[00:09]你让我心神不宁
+[00:09]Oooh
+[00:11]哦哦
+[00:11]It's like somebody got inside me
+[00:13]那感觉就像心里藏了一个人
+[00:13]Dancing under my skin yeah
+[00:15]蠢蠢欲动
+[00:15]All my people
+[00:17]我周围的姐妹
+[00:17]All my people
+[00:19]我周围的姐妹
+[00:19]Know everything about you
+[00:23]她们都对你很了解
+[00:23]But all my people
+[00:24]但是我的姐妹们
+[00:24]All my people
+[00:26]我周围的姐妹
+[00:26]Didn't know that I mean to you
+[00:31]却不知道你我的关系
+[00:31]I think this virus got my baby
+[00:34]我想我是中了爱情的毒
+[00:34]I need a doctor for my baby
+[00:38]我需要医生
+[00:38]He's gon bad he's gon bad
+[00:40]他真坏，他真坏
+[00:40]He's gon bad he's gon bad
+[00:41]他真坏，他真坏
+[00:41]He's gon bad he's gon bad
+[00:43]他真坏，他真坏
+[00:43]He's gon bad he's gon bad
+[00:45]他真坏，他真坏
+[00:45]Oh oh oh oooh
+[00:48]哦哦哦哦
+[00:48]Sometimes I feel you on my body
+[00:50]我总能感觉的到你的存在
+[00:50]You make my head spin yeah
+[00:53]你让我心神不宁
+[00:53]Oh oh oh oooh
+[00:56]哦哦哦哦
+[00:56]It's like somebody got inside me
+[00:58]那感觉就像心里藏了一个人
+[00:58]Dancing under my skin yeah
+[01:00]蠢蠢欲动
+[01:00]All my people
+[01:02]我周围的姐妹
+[01:02]All my people
+[01:04]我周围的姐妹
+[01:04]Know everything about you
+[01:06]她们都对你很了解
+[01:06]Everything about you
+[01:08]她们都对你很了解
+[01:08]But all my people
+[01:09]但是我的姐妹们
+[01:09]All my people
+[01:11]我的姐妹们
+[01:11]Didn't know that I mean to you
+[01:30]却不知道你我的关系
+[01:30]Thank all my people
+[01:32]但是我还是要谢谢她们
+[01:32]Dancing as I sing
+[01:34]我边唱边跳
+[01:34]Everybody's moving to the rhythm that I bring
+[01:38]周围的人都跟着我的节拍
+[01:38]Sing all my people dancing as I sing
+[01:42]我与她们载歌载舞
+[01:42]Everybody's moving to the rhythm that I bring
+[01:45]周围的人都跟着我的节拍
+[01:45]I'm watching you dancing
+[01:47]看着你在跳舞
+[01:47]I'm out of control
+[01:49]我失去自我
+[01:49]Cause I see right through me
+[01:50]因为我发现你也在看我
+[01:50]He's staring into my soul whoah
+[01:56]目光直抵我的心底
+[01:56]And all the people you knew
+[01:58]你认识她们
+[01:58]They didn't know about you
+[02:00]但她们却不了解你
+[02:00]All my people
+[02:02]我周围的姐妹
+[02:02]All my people
+[02:04]我周围的姐妹
+[02:04]Know everything about you yeah
+[02:08]她们都对你很了解
+[02:08]All my people
+[02:09]我周围的姐妹
+[02:09]All my people
+[02:11]我周围的姐妹
+[02:11]Didn't know that I mean to you
+[02:14]却不知道你我的关系
+[02:14]I mean to you
+[02:15]我对于你意味着什么
+[02:15]All my people
+[02:17]我周围的姐妹
+[02:17]All my people
+[02:19]我周围的姐妹
+[02:19]Know everything about you
+[02:21]她们都对你很了解
+[02:21]Everything about you
+[02:23]知道你的一切
+[02:23]All my people
+[02:24]我周围的姐妹
+[02:24]All my people
+[02:26]我周围的姐妹
+[02:26]Didn't know that I mean to you
+[02:30]却不知道你我的关系
+[02:30]Yeah yeah yeah yeah yeah yeah
+[02:45]是是是是
+[02:45]All my people
+[02:47]我周围的姐妹
+[02:47]All my people
+[02:53]我周围的姐妹
+[02:53]But all my people
+[02:54]可是我周围的姐妹
+[02:54]All my people
+[03:00]我周围的姐妹
+[03:00]All my people
+[03:02]我周围的姐妹
+[03:02]All my people
+[03:04]我周围的姐妹
+[03:04]Know everything about you
+[03:07]她们都对你很了解
+[03:07]Everything about you
+[03:08]知道你的一切
+[03:08]All my people
+[03:09]我周围的姐妹
+[03:09]All my people
+[03:11]我周围的姐妹
+[03:11]Didn't know that I mean to you
+[03:14]却不知道你我的关系`
